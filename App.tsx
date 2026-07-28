c1cb29376c20ae709bfedffdacfaf22332c2d30e
sapna179 <64653889+sapna179@users.noreply.github.com>
2026-07-28 17:25:11 +0530

    Native Local Storage

--- App.tsx
+++ App.tsx
@@ -1,44 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import {useEffect, useState, type JSX} from 'react';
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+} from 'react-native';
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+import NativeLocalStorage from './specs/NativeLocalStorage';
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
+const EMPTY = '<empty>';
+
+function App(): JSX.Element {
+  const [value, setValue] = useState<string | null>(null);
+
+  const [editingValue, setEditingValue] = useState<string | null>(
+    null,
   );
-}
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  useEffect(() => {
+    const storedValue = NativeLocalStorage?.getItem('myKey');
+    setValue(storedValue ?? '');
+  }, []);
+
+  function saveValue() {
+    NativeLocalStorage?.setItem(editingValue ?? EMPTY, 'myKey');
+    setValue(editingValue);
+  }
+
+  function clearAll() {
+    NativeLocalStorage?.clear();
+    setValue('');
+  }
+
+  function deleteValue() {
+    NativeLocalStorage?.removeItem('myKey');
+    setValue('');
+  }
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+    <SafeAreaView style={{flex: 1}}>
+      <Text style={styles.text}>
+        Current stored value is: {value ?? 'No Value'}
+      </Text>
+      <TextInput
+        placeholder="Enter the text you want to store"
+        style={styles.textInput}
+        onChangeText={setEditingValue}
       />
-    </View>
+      <Button title="Save" onPress={saveValue} />
+      <Button title="Delete" onPress={deleteValue} />
+      <Button title="Clear" onPress={clearAll} />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  text: {
+    margin: 10,
+    fontSize: 20,
+  },
+  textInput: {
+    margin: 10,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    paddingLeft: 5,
+    paddingRight: 5,
+    borderRadius: 5,
   },
 });
 
---
+++ ios/NativeLocalStorage/RCTNativeLocalStorage.h
@@ -0,0 +1,17 @@
+//
+//  RCTNativeLocalStorage.h
+//  TurboModuleExample
+//
+//  Created by Sapna Devi on 28/07/26.
+//
+
+#import <Foundation/Foundation.h>
+#import <NativeLocalStorageSpec/NativeLocalStorageSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RCTNativeLocalStorage : NSObject <NativeLocalStorageSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
